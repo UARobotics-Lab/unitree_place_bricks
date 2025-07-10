85477bfafb01e4de924c168f66a149d922706a7c
sarahgne <s.neirav@uniandes.edu.co>
2025-07-10 18:49:35 -0500

    Se hizo una linea que guarda los pasos en un archivo .npy

--- IK/aura_test.py
+++ IK/aura_test.py
@@ -132,6 +132,8 @@ q_LM, success_LM, its_LM, searches_LM, E_LM, jl_valid_LM, t_LM, q_steps_LM = sol
 # --- Guardar q_LM ---
 np.save("q_LM_result.npy", q_LM)
 print("q_LM guardado en archivo q_LM_result.npy")
+np.save("q_steps_LM.npy", q_steps_LM)
+print("q_steps_LM guardado en archivo q_steps_LM.npy")
 
 
 print(" IK con LM:")
